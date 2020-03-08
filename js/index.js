e8ee4a606e210d3b6e85b717f53abefdf5b47d77
25gmax <55412802+25gmax@users.noreply.github.com>
2020-03-07 22:23:46 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -29,14 +29,11 @@ function includeHTML() { //Thanks w3!
     }
   }
 };
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginRight = "250px";
-}
-
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginRight = "0";
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
